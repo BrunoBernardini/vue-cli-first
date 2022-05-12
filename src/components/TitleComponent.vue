5df95b74dcac54b5b7d8c4ac893dc92fda694512
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-05-12 17:21:53 +0200

    Componente title.

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>La mia prima app con Vue CLI!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  header{
+    background-color: aliceblue;
+    padding: 30px;
+    text-align: center;
+  }
+</style>
